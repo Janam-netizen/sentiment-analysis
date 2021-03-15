f6c497cf834d543477417d225d8df68217349dd6
Janam-netizen <68682774+Janam-netizen@users.noreply.github.com>
2021-03-16 00:51:39 +0400

    Update app.py
    Formatting code

--- app.py
+++ app.py
@@ -11,12 +11,7 @@ from flask import Flask
 from textblob import TextBlob
 from  matplotlib import pyplot as plt
 app = Flask(__name__)
-'''class Tweet():
-    
-    def __init__(self,post,timestamp):
-        self.post=post
-        self.timestamp=timestamp
-        self.score=TextBlob(post).sentiment.polarity'''
+
 
 
 
@@ -34,30 +29,8 @@ def get_score():
     print(text)
     score=TextBlob(text).sentiment.polarity
     return str(score)
-'''@app.route('/plot.png')
-def plot_png():
-    fig = create_figure()
-    output = io.BytesIO()
-    FigureCanvas(fig).print_png(output)
-    return Response(output.getvalue(), mimetype='image/png')
-
-def create_figure():
-    fig = Figure()
-    axis = fig.add_subplot(1, 1, 1)
-    twitter_data = get_data()
-    xs = twitter_data["timestamps"]
-    ys = twitter_data["scores"]
-    axis.plot(xs, ys)
-    return fig'''
+'
 
 app.run(debug=True)
 
-'''@app.route('/')
-def hello_world():
-    return render_template("user-form.html")
-@app.route('/graph')
-def get_graph():
-    handle=request.args.get("handle")
-    print(handle)
-    return render_template('graph.html',geocode=[1,5,6,7,8])
-(debug=True)'''
+
